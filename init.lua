9d2a1895acb82d7d32ab47e4cb8744cc3a445c55
jimrothstein <jimrothstein@gmail.com>
2023-07-22 14:40:45 -0700

    resetting options.lua to be DEFAULT, provided by lazyvim;   except for my changes

--- init.lua
+++ init.lua
@@ -4,5 +4,6 @@ require("config.lazy")
 -- NOTES:
 -- require("config")                  should run ~/.config/nvim/lua/config/init.lua
 -- require("bees")                  -- will run ~/.config/nvim/lua/bees.lua -- :use messages to see
-require("config.bees") -- should run ~/.config/nvim/lua/config/bees.lua
-require("config.luasnip")
+--
+require("config.bees") -- runs ~/.config/nvim/lua/config/bees.lua
+require("config.luasnip") -- runs ~/.config/nvim/lua/config/luasnip.lua (my snips)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,28 +1,19 @@
 -- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
 
---  14June 2023
---  TODO
---    -     mess, restructed ... need to restore to working conditin
---    -     cmp?  is hijacking <Tab>  - how to disable, REF:  https://github.com/hrsh7th/nvim-cmp/issues/880
---
+----------
+--  Nvim-R
+----------
 vim.cmd([[ let R_assign=2 ]])
 vim.cmd([[ let R_enable_comment = 1 ]]) -- toggle comments <leader>xx
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
---
+vim.g.R_assign = 2 --  turn off netrw (`let g:loaded_netrw = 1`)
 --
 --  REF         ThePrimeagen
 --
 if false then
   print("hello: nvim/config/options.lua")
 end
---          PUT jr options here !:
---
---
 
 -- $HOST is shell environmental var, set by initialize.zsh
 local HOST = os.getenv("HOST") --- which machine?
@@ -37,8 +28,6 @@ vim.g.mapleader = " "
 -- remains block in insert mode
 
 vim.opt.numberwidth = 3
-vim.opt.relativenumber = true -- test only, already set to true by lazyv
-vim.opt.number = true
 
 if HOST == "jim-ThinkPad-T480" then
   vim.cmd([[ set background=light]])
@@ -46,23 +35,18 @@ else
   vim.cmd([[ set background=dark]]) -- acer-desktop
 end
 
-vim.cmd([[ set clipboard+=unnamedplus]])
+-- leave default (set clipboard=unnamedplus)
+-- vim.cmd([[ set clipboard+=unnamedplus]])
+
 vim.cmd([[ syntax on]])
 vim.g.listchars = "tab:>-,space:="
 
 vim.opt.conceallevel = 0 -- show chunk markers
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
--- nvim-cmp  sets, don't touch till have good reason
+vim.opt.tabstop = 2
+-- default:  omits this
+--vim.opt.softtabstop = 4
 --
--- Set completeopt to have a better completion experience
--- vim.cmd([[set completeopt=menuone,noinsert,noselect]])
--- another says
--- vim.opt.completeopt = { "menu", "menuone", "noselect" }
---
+vim.opt.shiftwidth = 2
 
 --
 
@@ -74,17 +58,6 @@ vim.opt.incsearch = true
 -- keep 8
 vim.opt.scrolloff = 8
 vim.opt.colorcolumn = "80"
---  TODO
-
-vim.g.R_assign = 2 --  turn off netrw (`let g:loaded_netrw = 1`)
-vim.g.loaded_netrw = 0
-vim.g.loaded_netrwPlugin = 0
-
---  load luasnip?
-local ls = require("luasnip")
-
---  load mini.surround
-require("mini.surround")
 
 --  practice function
 local function nohl_or_buffer_cycle()
@@ -97,3 +70,11 @@ local function nohl_or_buffer_cycle()
 end
 
 vim.keymap.set("n", "<C-l>", nohl_or_buffer_cycle)
+
+-----------------------------------------
+--      Below, default options (REF ONLY)
+-----------------------------------------
+--
+if true then
+  return {}
+end
