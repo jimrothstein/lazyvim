28b5444868bf7e68ce241834f2a06b87d6b11cf3
jimrothstein <jimrothstein@gmail.com>
2023-07-26 16:39:10 -0700

    using mason and autocmd from directions/   bash LSP seems to be working

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,3 +3,13 @@
 -- Add any additional autocmds here
 --
 --
+-- base language server: https://github.com/bash-lsp/bash-language-server#neovim
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "sh",
+  callback = function()
+    vim.lsp.start({
+      name = "bash-language-server",
+      cmd = { "bash-language-server", "start" },
+    })
+  end,
+})
