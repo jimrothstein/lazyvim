8f4c52a95766af1907117b7deed67f412329b22e
jimrothstein <jimrothstein@gmail.com>
2023-07-27 00:11:28 -0700

    first commit of quarto_dev branch;  can I get quarto-nvim-kickstarter to WORK??

---
+++ lua/misc/style.lua
@@ -0,0 +1,7 @@
+-- /home/jim/.config/nvim/lua/misc/style.lua
+-- PUROSE:  quarto.lua plugin needs this
+local M = {}
+
+M.border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
+
+return M
---
+++ lua/plugins/lsp_config.lua
@@ -0,0 +1,6 @@
+-- /home/jim/.config/nvim/lua/plugins/lsp_config.lua
+-- purpose:  put LSP here (till I understand things better)
+--
+--
+-- nvim-lspconfig provideds :LspInfo tool
+return { "neovim/nvim-lspconfig" }
