f7a1d05619257628bb555c4bef214a07b4cc2a3c
jimrothstein <jimrothstein@gmail.com>
2023-07-27 17:00:32 -0700

    R and which-key conflict ...now debugging ... removed code in overrides.lua

--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -3,6 +3,10 @@
 -- Which-Key  - this adds features.  seems to work ok.
 -- Anything does not work?    Just remoove this file.
 --
+if true then
+  return {}
+end
+
 local Util = require("lazyvim.util")
 
 return {
