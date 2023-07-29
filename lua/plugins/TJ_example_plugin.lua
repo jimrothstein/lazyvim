e694866c7b8af75d51760dabc05a45256999a71d
jimrothstein <jimrothstein@gmail.com>
2023-07-29 16:55:51 -0700

    TJ:   video to create 1st plugin

--- lua/plugins/TJ_example_plugin.lua
+++ lua/plugins/TJ_example_plugin.lua
@@ -1,1 +1,25 @@
 -- https://www.youtube.com/watch?v=n4Lp4cV8YR0&t=1951s
+-- TJ Plugin example:   he calls  it `mapstack`
+-- vim.api package/class
+-- functions:
+--    nvim_set_keymap()
+--    nvim_get_keymap()
+--
+--
+--
+--    vim.api
+local M = {}
+-- USAGE:   require(`mapstack`).push()
+M.push = function(name, mappings) end
+
+M.pop = function(name) end
+
+--  :x,y source
+--
+-- usage:  :lua P({key = "value"}), will print key=value
+P = function(v)
+  print(vim.inspect(v))
+  return v
+end
+
+return M
